0cb39128577c9e911dae1af572381900e0969286
Victoria <vkvieira@yahoo.com>
2024-03-13 17:58:41 -0300

    :sparkles: feat: página/componente login criado

--- lavanderia/src/main.ts
+++ lavanderia/src/main.ts
@@ -1,6 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
