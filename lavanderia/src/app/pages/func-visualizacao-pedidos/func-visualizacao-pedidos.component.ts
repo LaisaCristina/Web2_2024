21f68906c53fc44d9e45a4b8f8f575796b591de2
vickvieira <108843670+vickvieira@users.noreply.github.com>
2024-06-26 19:35:22 -0300

    Revert ":sparkles: feat: modal implementado na pag func-visualização-pedidos …"

--- lavanderia/src/app/pages/func-visualizacao-pedidos/func-visualizacao-pedidos.component.ts
+++ lavanderia/src/app/pages/func-visualizacao-pedidos/func-visualizacao-pedidos.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ConfirmationModalComponent } from '../../components/confirmation-modal/confirmation-modal.component';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-func-visualizacao-pedidos',
   templateUrl: './func-visualizacao-pedidos.component.html',
   styleUrls: ['./func-visualizacao-pedidos.component.css']
 })
-export class FuncVisualizacaoPedidosComponent implements OnInit {
-  @ViewChild('confirmationModal') confirmationModal!: ConfirmationModalComponent;
+export class FuncVisualizacaoPedidosComponent // visualizacao-pedidos.component.ts
 
+{
   pedidos = [
     { numero: 1, dataHora: '2024-03-28 10:00', estado: 'EM ABERTO' },
     { numero: 2, dataHora: '2024-03-27 11:00', estado: 'REJEITADO' },
@@ -20,55 +19,6 @@ export class FuncVisualizacaoPedidosComponent implements OnInit {
 
   filtroDataInicio: string = '';
   filtroDataFim: string = '';
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  confirmarAcao(pedido: any, acao: string) {
-    let title = '';
-    let message = '';
-  
-    switch (acao) {
-      case 'recolhimento':
-        title = 'Confirmar Recolhimento';
-        message = 'Tem certeza que deseja confirmar o recolhimento deste pedido?';
-        break;
-      case 'lavagem':
-        title = 'Confirmar Lavagem';
-        message = 'Tem certeza que deseja confirmar a lavagem deste pedido?';
-        break;
-      case 'finalizacao':
-        title = 'Finalizar Pedido';
-        message = 'Tem certeza que deseja finalizar este pedido?';
-        break;
-      default:
-        break;
-    }
-  
-    this.confirmationModal.title = title;
-    this.confirmationModal.message = message;
-    this.confirmationModal.showModal();
-  
-    this.confirmationModal.confirmAction.subscribe((confirmed: boolean) => {
-      if (confirmed) {
-        switch (acao) {
-          case 'recolhimento':
-            this.confirmarRecolhimento(pedido);
-            break;
-          case 'lavagem':
-            this.confirmarLavagem(pedido);
-            break;
-          case 'finalizacao':
-            this.finalizarPedido(pedido);
-            break;
-          default:
-            break;
-        }
-      }
-    });
-  }
 
   filtrarPorData() {
     // Implemente a lógica de filtragem por data
